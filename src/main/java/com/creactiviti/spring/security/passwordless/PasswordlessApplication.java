19e8a958c2532a00d634eef31b94504cbf8b5c65
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2018-02-04 17:51:34 -0500

    refactoring.

--- src/main/java/com/creactiviti/spring/security/passwordless/PasswordlessApplication.java
+++ src/main/java/com/creactiviti/spring/security/passwordless/PasswordlessApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import com.creactiviti.spring.security.passwordless.core.Authenticator;
 import com.creactiviti.spring.security.passwordless.core.EmailSender;
 import com.creactiviti.spring.security.passwordless.core.InMemoryTokenStore;
 import com.creactiviti.spring.security.passwordless.core.Sender;
+import com.creactiviti.spring.security.passwordless.core.SpringSecurityAuthenicator;
 import com.creactiviti.spring.security.passwordless.core.TokenStore;
 
 @SpringBootApplication
@@ -25,6 +27,11 @@ public class PasswordlessApplication {
   @Bean
   Sender sender (JavaMailSender aJavaMailSender) {
     return new EmailSender(aJavaMailSender);
+  }
+  
+  @Bean
+  Authenticator authenticator () {
+    return new SpringSecurityAuthenicator(tokenStore());
   }
 
 }
